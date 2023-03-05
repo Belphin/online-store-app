1ff8405f3dc5b4eb03364816f212c38cec4bdb96
Belphin <findolph0003@gmail.com>
2023-03-05 16:11:23 +0200

    error handler

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const ApiError = require("../error/ApiError");
+
 class UserController {
 	async registration(req, res) {
 		try {
@@ -7,8 +9,13 @@ class UserController {
 		try {
 		} catch (e) {}
 	}
-	async check(req, res) {
+	async check(req, res, next) {
 		try {
+			const { id } = req.query;
+			if (!id) {
+				return next(ApiError.badRequest("id not set"));
+			}
+			res.json(id);
 		} catch (e) {}
 	}
 }
