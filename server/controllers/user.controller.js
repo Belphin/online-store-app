79c618e0d589d99ed6b1f736cb6c8b3e4f446c05
Belphin <findolph0003@gmail.com>
2023-03-06 22:42:12 +0200

    auth

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,22 +1,47 @@
 const ApiError = require("../error/ApiError");
+const { User, Basket } = require("../models/models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const generateJwt = (id, email, role) =>
+	jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+		expiresIn: "24h",
+	});
 
 class UserController {
-	async registration(req, res) {
-		try {
-		} catch (e) {}
+	async registration(req, res, next) {
+		const { email, password, role } = req.body;
+		if (!email || !password) {
+			return next(ApiError.badRequest("Invalid email or password"));
+		}
+		const candidate = await User.findOne({ where: { email } });
+		if (candidate) {
+			return next(ApiError.badRequest("Email already registered"));
+		}
+		const hashPassword = bcrypt.hashSync(password, 5);
+		const user = await User.create({ email, password: hashPassword, role });
+		await Basket.create({ userId: user.id });
+		const token = generateJwt(user.id, user.email, user.role);
+		return res.json({ token });
 	}
-	async login(req, res) {
-		try {
-		} catch (e) {}
+
+	async login(req, res, next) {
+		const { email, password } = req.body;
+		const user = await User.findOne({ where: { email } });
+		if (!user) {
+			return next(ApiError.badRequest("Invalid email or password"));
+		}
+		const comparePassword = bcrypt.compareSync(password, user.password);
+		if (!comparePassword) {
+			return next(ApiError.badRequest("Invalid email or password"));
+		}
+		const token = generateJwt(user.id, user.email, user.role);
+		return res.json({ token });
 	}
+
 	async check(req, res, next) {
-		try {
-			const { id } = req.query;
-			if (!id) {
-				return next(ApiError.badRequest("id not set"));
-			}
-			res.json(id);
-		} catch (e) {}
+		const token = generateJwt(req.user.id, req.user.email, req.uesr.role);
+		return res.json({ token });
 	}
 }
 
--- server/routes/brand.router.js
+++ server/routes/brand.router.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const brandController = require("../controllers/brand.controller");
+const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
 const router = new Router();
 
-router.post("/", brandController.create);
+router.post("/", checkRoleMiddleware("ADMIN"), brandController.create);
 router.get("/", brandController.getAll);
 
 module.exports = router;
